ce835dbbd0f8323f3bdbc5b7cb8f413864dd4eda
puthier <puthier@gmail.com>
2018-09-26 17:36:27 +0200

    Setup now reads README.rst

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ if os.path.exists(gtftk_cnf_dir):
         sys.stderr.write("Failed to delete .gtftk directory.\n")
 else:
     sys.stderr.write("No .gtftk directory found.\n")
+
 # -------------------------------------------------------------------------
 # Check gtftk version
 # -------------------------------------------------------------------------
@@ -128,51 +129,10 @@ lib_pygtftk = Extension(name='pygtftk/lib/libgtftk',
                         sources=cmd_src_list)
 
 # ----------------------------------------------------------------------
-# Delete the first line from REAME.md
-# and convert .md to .rst...
-# This is required for Pypi that use rst format
-# and will read the content of the long_description key
+# Description
 # ----------------------------------------------------------------------
-# I just realized that github also support rst...
-# This part should be deleted in the future.
 
-long_description_file = open('README.md')
-long_description = []
-markup_char = {1: "=", 2: "-", 3: "~"}
-markup_level = 0
-past_line_len = 0
-
-start_of_text = False
-
-for pos, line in enumerate(long_description_file):
-    if not line.startswith("    "):
-        line = line.rstrip('\r\n')
-    if line.startswith("# ") and start_of_text is False:
-        start_of_text = True
-
-    if start_of_text:
-        # Replace title
-        if markup_level > 0:
-            title = markup_char[markup_level] * past_line_len
-            long_description += [title]
-        len_line = len(line)
-        line = re.sub("^#+", "", line)
-        markup_level = len_line - len(line)
-        past_line_len = len(line)
-
-        # replace URL
-        for hit in re.finditer("\[(.*?)\]\((.*?)\)", line):
-            line = line.replace("[" + hit.group(1) + "]", "`" + hit.group(1) + " <")
-            line = line.replace("(" + hit.group(2) + ")", hit.group(2) + ">`_")
-
-        if markup_level:
-            long_description += [line.lstrip(" ")]
-        else:
-            long_description += [line]
-
-long_description = "\n".join(long_description)
-long_description_rst = open('README.rst', mode="w")
-long_description_rst.write(long_description)
+long_description = open('README.rst', mode="r").read()
 
 # ----------------------------------------------------------------------
 # Update the docs/manual/source/conf.py
@@ -314,6 +274,7 @@ try:
 except OSError as e:
     pass
 
+
 sys.stderr.write("Installation complete.\n")
-result = subprocess.run(['gtftk', '-h'], stdout=subprocess.PIPE)
-sys.stderr.write(result.stdout.decode() + "\n")
+result = subprocess.Popen(['gtftk', '-h'], stdout=subprocess.PIPE)
+sys.stderr.write(result.stdout.read().decode() + "\n")
