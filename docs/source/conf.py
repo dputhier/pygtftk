d5cbe1d90867d2a30f19b7485d208943b984482c
puthier <puthier@gmail.com>
2018-09-28 11:03:05 +0200

    No img associated to html_extra_path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -130,7 +130,7 @@ html_theme = u'nature'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = [u'_static']
-html_static_path = [u'_static', 'img']
+html_static_path = [u'_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -300,4 +300,4 @@ html_theme_options = {
 
 # html_sidebars = {'**': ['localtoc.html', 'sourcelink.html', 'searchbox.html']}
 
-html_extra_path = ['img']
+html_extra_path = ['_static']
