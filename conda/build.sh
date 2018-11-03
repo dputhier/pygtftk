65559d323353e2d3f70224b95fc0d977ead9f0f4
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2018-11-03 13:40:16 +0100

    update conda build for 0.9.8

--- conda/build.sh
+++ conda/build.sh
@@ -1,3 +1,4 @@
 ln -s $CC `dirname $CC`/gcc
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON -m pip install --no-deps --ignore-installed --no-cache-dir -vvv .
+#$PYTHON setup.py install --single-version-externally-managed --record=record.txt
 cp bin/* $PREFIX/bin/
