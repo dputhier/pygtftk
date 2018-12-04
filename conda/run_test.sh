37c2f492552878012f581d39a04189cf9e8bb2cc
Denis Puthier <denis.puthier@univ-amu.fr>
2018-12-04 16:36:14 +0100

    fixing conda build

--- conda/run_test.sh
+++ conda/run_test.sh
@@ -8,7 +8,7 @@ mkdir -p gtftk_test
 cd gtftk_test
 a=`python -c "import os,pygtftk; print(os.path.dirname(pygtftk.__file__))"`
 cd $a
-for i in `find . -name "*.py" | perl -ne  'print unless(/(setup)|(plugin)|(libgtftk.py)|(__)/)'`
+for i in `find . -name "*.py" | perl -ne  'print unless(/(setup)|(plugin)|(bwig)|(libgtftk.py)|(__)/)'`
 do 
 	echo "================="
 	echo $i
@@ -18,4 +18,3 @@ done
 gtftk -p > gtftk_test.bats
 bats -t gtftk_test.bats
 rm -f gtftk_test.bats
-
