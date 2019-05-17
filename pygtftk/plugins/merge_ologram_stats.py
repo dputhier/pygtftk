c162eced8b3e346fd32a5e657ac9a028479d2933
Denis Puthier <denis.puthier@univ-amu.fr>
2019-05-17 11:00:09 +0200

    Add dataset ologram_1

---
+++ pygtftk/plugins/merge_ologram_stats.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+"""
+Merge a set of OLOGRAM outputs into a single output. Build a heatmap from the results.
+"""
+
+import argparse
+import os
+import warnings
+
+import numpy as np
+import pandas as pd
+from plotnine import (ggplot, aes, geom_tile, theme_bw,
+                      element_blank, theme,
+                      element_text, save_as_pdf_pages)
+
+from pygtftk import arg_formatter
+from pygtftk.cmd_object import CmdObject
+from pygtftk.utils import message
+
+__updated__ = '''  '''
+
+__notes__ = """
+"""
+
+
+def make_parser():
+    """The main argument parser."""
+    parser = argparse.ArgumentParser(add_help=True)
+
+    parser_grp = parser.add_argument_group('Arguments')
+
+    parser_grp.add_argument('inputfiles',
+                            help="Path to the OLOGRAM output files",
+                            type=arg_formatter.FormattedFile(mode='r', file_ext=('txt')),
+                            nargs='+')
+
+    parser_grp.add_argument('-pw', '--pdf-width',
+                            help='Output pdf file width (inches).',
+                            type=arg_formatter.ranged_num(0, None),
+                            default=None,
+                            required=False)
+
+    parser_grp.add_argument('-ph', '--pdf-height',
+                            help='Output pdf file height (inches).',
+                            type=arg_formatter.ranged_num(0, None),
+                            default=None,
+                            required=False)
+
+    parser_grp.add_argument('-o', '--output',
+                            help="Pdf file name. ",
+                            default=None,
+                            nargs=None,
+                            type=arg_formatter.FormattedFile(mode='w', file_ext='pdf'),
+                            required=True)
+
+    return parser
+
+
+def merge_ologram_stats(inputfiles=None,
+                        pdf_width=None,
+                        pdf_height=None,
+                        output=None):
+    # -------------------------------------------------------------------------
+    # Loop over input files
+    # -------------------------------------------------------------------------
+
+    df_list = list()
+    df_label = list()
+
+    for pos, infile in enumerate(inputfiles):
+        message("Reading file : " + infile.name)
+        file_short_name = os.path.dirname(infile.name)
+        df_label += [file_short_name]
+        # read the dataset
+        df_tmp = pd.read_csv(infile, sep='\t', header=0, index_col=None)
+        # Change name of 'feature_type' column.
+        df_tmp = df_tmp.rename(index=str, columns={"feature_type": "Feature"})
+        # Assign the name of the dataset to a new column
+        df_tmp = df_tmp.assign(**{"dataset": [file_short_name] * df_tmp.shape[0]})
+        # Pval set to 0 are changed to  1e-320
+        df_tmp.loc[df_tmp['summed_bp_overlaps_pvalue'] == 0, 'summed_bp_overlaps_pvalue'] = 1e-320
+        # Pval set to 0 are changed to  1e-320
+        df_tmp.loc[df_tmp['summed_bp_overlaps_pvalue'] == -1, 'summed_bp_overlaps_pvalue'] = np.nan
+        # Compute -log10(pval)
+        df_tmp = df_tmp.assign(**{"-log_10(pval)": -np.log10(df_tmp.summed_bp_overlaps_pvalue)})
+        # Add the df to the list to be subsequently merged
+        df_list += [df_tmp]
+
+    # -------------------------------------------------------------------------
+    # Concatenate dataframes (row bind)
+    # -------------------------------------------------------------------------
+
+    message("Merging dataframes.")
+    df_merged = pd.concat(df_list, axis=0)
+
+    # -------------------------------------------------------------------------
+    # Plotting
+    # -------------------------------------------------------------------------
+
+    message("Plotting")
+    my_plot = ggplot(data=df_merged,
+                     mapping=aes(y='Feature',
+                                 fill='-log_10(pval)',
+                                 x='dataset'))
+
+    my_plot += geom_tile()
+
+    # theming
+    my_plot += theme_bw()
+    my_plot += theme(panel_grid_major=element_blank(),
+                     axis_text_x=element_text(rotation=90),
+                     panel_border=element_blank(),
+                     axis_ticks=element_blank())
+
+    # -------------------------------------------------------------------------
+    # Saving
+    # -------------------------------------------------------------------------
+
+    message("Saving")
+    nb_ft = len(list(df_merged['Feature'].unique()))
+    nb_datasets = len(list(df_merged['dataset'].unique()))
+
+    if pdf_width is None:
+        panel_width = 0.6
+        pdf_width = panel_width * nb_datasets
+
+        if pdf_width > 100:
+            pdf_width = 100
+            message("Setting --pdf-width to 100 (limit)")
+
+    if pdf_height is None:
+        panel_height = 0.6
+        pdf_height = panel_height * nb_ft
+
+        if pdf_height > 100:
+            pdf_height = 100
+            message("Setting --pdf-height to 100 (limit)")
+
+    message("Page width set to " + str(pdf_width))
+    message("Page height set to " + str(pdf_height))
+    figsize = (pdf_width, pdf_height)
+
+    # -------------------------------------------------------------------------
+    # Turn warning off. Both pandas and plotnine use warnings for deprecated
+    # functions. I need to turn they off although I'm not really satisfied with
+    # this solution...
+    # -------------------------------------------------------------------------
+
+    def fxn():
+        warnings.warn("deprecated", DeprecationWarning)
+
+    # -------------------------------------------------------------------------
+    # Saving
+    # -------------------------------------------------------------------------
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        fxn()
+
+        message("Saving diagram to file : " + output.name)
+        message("Be patient. This may be long for large datasets.")
+
+        # NOTE : We must manually specify figure size with save_as_pdf_pages
+        save_as_pdf_pages(filename=output.name,
+                          plots=[my_plot + theme(figure_size=figsize)],
+                          width=pdf_width,
+                          height=pdf_height)
+
+
+def main():
+    """The main function."""
+
+    myparser = make_parser()
+    args = myparser.parse_args()
+    args = dict(args.__dict__)
+    merge_ologram_stats(**args)
+
+
+if __name__ == '__main__':
+    main()
+
+
+else:
+
+    # 'Bats' tests
+    test = '''
+#ologram: get example files
+@test "ologram_0" {
+     result=`gtftk get_example -d simple_02 -f '*'`
+  [ "$result" = "" ]
+}
+'''
+
+    cmd = CmdObject(name="merge_ologram_stats",
+                    message="Build a heatmap from several ologram output files (tsv).",
+                    parser=make_parser(),
+                    fun=os.path.abspath(__file__),
+                    desc=__doc__,
+                    group="annotation",
+                    notes=__notes__,
+                    updated=__updated__,
+                    test=test)
