00fac1c138b9c0310392e7ae371b3573446e3145
Denis Puthier <denis.puthier@univ-amu.fr>
2019-06-21 10:11:59 +0200

    added --compute-dist

--- pygtftk/plugins/tss_numbering.py
+++ pygtftk/plugins/tss_numbering.py
@@ -40,9 +40,19 @@ def make_parser():
                             metavar="TXT",
                             type=arg_formatter.FormattedFile(mode='w', file_ext=('gtf')))
 
-    parser_grp.add_argument('-d', '--dest-key',
+    parser_grp.add_argument('-k', '--key-name',
                             help="The name of the new key.",
                             default='tss_number',
+                            type=str,
+                            required=False)
+
+    parser_grp.add_argument('-c', '--compute-dist',
+                            help="Add a key indicating the distance to the first tss (the most 5').",
+                            action='store_true')
+
+    parser_grp.add_argument('-d', '--key-name-dist',
+                            help="If --compute-dist is true a name for that key.",
+                            default='dist_to_first_tss',
                             type=str,
                             required=False)
 
@@ -52,7 +62,9 @@ def make_parser():
 def tss_numbering(
         inputfile=None,
         outputfile=None,
-        dest_key='tss_number'):
+        compute_dist=False,
+        key_name='tss_number',
+        key_name_dist='dist_to_first_tss'):
     """
     Computes the distance between TSS of gene transcripts.
     """
@@ -62,12 +74,17 @@ def tss_numbering(
     gn_tss_dist = defaultdict(dict)
 
     message("Getting TSSs.")
-    tss = gtf.get_tss(name=["transcript_id", "gene_id"], as_dict=True)
+    tss = gtf.get_tss(name=["transcript_id"], as_dict=True)
+    tx_to_gn = gtf.get_tx_to_gn()
 
     for k in tss:
-        tx_id, gn_id = k.split("|")
-        gn_tss_dist[gn_id][tx_id] = int(tss[k])
+        gn_id = tx_to_gn[k]
+        gn_tss_dist[gn_id][k] = int(tss[k])
 
+    # if_dict_of_dict is true, get_gn_to_tx() returns a dict of dict
+    # that maps gene_id to transcript_id and transcript_id to TSS
+    # numbering (1 for most 5', then 2...). For transcripts having
+    # the same TSSs, the tss number will be the same.
     gn_to_tx_to_tss = gtf.get_gn_to_tx(as_dict_of_dict=True)
 
     message("Numbering TSSs.")
@@ -78,14 +95,35 @@ def tss_numbering(
         for tx_id in gn_to_tx_to_tss[gn_id]:
             tss_num = str(gn_to_tx_to_tss[gn_id][tx_id])
             tss_number_file.write(tx_id + "\t" + tss_num + "\n")
-
     tss_number_file.close()
 
     gtf = gtf.add_attr_from_file(feat='transcript',
                                  key='transcript_id',
-                                 new_key=dest_key,
+                                 new_key=key_name,
                                  inputfile=open(tss_number_file.name),
                                  has_header=False)
+
+    if compute_dist:
+        gn_to_tx_ordered_by_tss = gtf.get_gn_to_tx(ordered_5p=True)
+        tss_dist_file = make_tmp_file(prefix='tx_tss_dist_to_first_tss', suffix='.txt')
+
+        for gn_id in gn_to_tx_to_tss:
+            tx_list = gn_to_tx_ordered_by_tss[gn_id]
+            tx_first = tx_list.pop(0)
+            # The first tss as distance 0 to the
+            # first tss...
+            tss_dist_file.write(tx_first + "\t0\n")
+            for tx_id in tx_list:
+                dist_to_first = abs(int(tss[tx_first]) - int(tss[tx_id]))
+                tss_dist_file.write(tx_id + "\t" + str(dist_to_first) + "\n")
+
+        tss_dist_file.close()
+
+        gtf = gtf.add_attr_from_file(feat='transcript',
+                                     key='transcript_id',
+                                     new_key=key_name_dist,
+                                     inputfile=open(tss_dist_file.name),
+                                     has_header=False)
 
     gtf.write(outputfile,
               gc_off=True)
@@ -108,19 +146,37 @@ else:
 
     test = """
         
-    #tss_dist 
+    #tss_numbering 
     @test "tss_numbering_0" {
      result=`gtftk get_example -d mini_real | gtftk select_by_key -k gene_id -v ENSG00000175756 | gtftk tss_numbering| gtftk select_by_key -t| gtftk tabulate -x -k transcript_id,start,end,strand,tss_number | md5sum-lite | perl -npe 's/\\s.*//'`
       [ "$result" = 'e01c4c1c167c8f2551584a3a6352b48b' ]
     }
 
-    #tss_dist 
+    #tss_numbering 
     @test "tss_numbering_1" {
      result=`gtftk get_example -d mini_real | gtftk select_by_key -k gene_id -v ENSG00000142611 | gtftk tss_numbering| gtftk select_by_key -t| gtftk tabulate -x -k transcript_id,start,end,strand,tss_number | md5sum-lite | perl -npe 's/\\s.*//'`
       [ "$result" = 'c4a9b29bd0385b57ed64cde6325b46fe' ]
     }
 
+    # G0011T001	250	267	-	2	13
+    # G0011T002	250	280	-	1	0
+    # tss_numbering 
+    @test "tss_numbering_2" {
+     result=`gtftk get_example -d simple_03 | gtftk tss_numbering -c | gtftk select_by_key -t | gtftk tabulate -x -k transcript_id,start,end,strand,tss_number,dist_to_first_tss | grep G0011 |md5sum-lite | perl -npe 's/\\s.*//'`
+      [ "$result" = '7e950a2fbe9c832736f1f6def27ea6de' ]
+    }
 
+
+    # G0002T001	180	189	+	1	0
+    # G0002T002	185	189	+	2	5
+    # tss_numbering 
+    @test "tss_numbering_2" {
+     result=`gtftk get_example -d simple_03 | gtftk tss_numbering -c | gtftk select_by_key -t | gtftk tabulate -x -k transcript_id,start,end,strand,tss_number,dist_to_first_tss | grep G0002 |md5sum-lite | perl -npe 's/\\s.*//'`
+      [ "$result" = '636ea1db16f5a44c3c6d83b46f372655' ]
+    }
+
+   
+    
     """
 
     CMD = CmdObject(name="tss_numbering",
