2c1c8d44ff3ee07536e8271dfb64b185f1e3c2ad
Quentin Ferré <quentin.q.ferre@gmail.com>
2021-04-01 12:25:46 +0200

    Removed accent from my name

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ if PY2:
 # -------------------------------------------------------------------------
 
 
-__author__ = 'Fabrice Lopez, Denis Puthier and Quentin Ferré'
+__author__ = 'Fabrice LOPEZ, Denis PUTHIER and Quentin FERRE'
 __email__ = 'denis.puthier@univ-amu.fr'
 __description__ = 'The Python GTF toolkit (pygtftk) package: easy handling of GTF files'
 __license__ = 'GPL-2'
