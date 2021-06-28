59ec6f7a5d21fca6ad7825323e156aa040503989
samantha.conte <samantha.conte@etu.univ-amu.fr>
2021-06-28 17:22:10 +0200

    html_diagram first commit

---
+++ pygtftk/plugins/html_diagram.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+This is the doc about the command that will appear when gtftk my_command -h
+is called...
+"""
+
+import sys
+import argparse
+from pygtftk.cmd_object import CmdObject
+from pygtftk import arg_formatter
